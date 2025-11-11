5d08dde8868ad4e553a827eb9310a4645aadf346
swuxz <sasha20070228@gmail.com>
2025-11-11 11:15:09 +0200

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -3,7 +3,16 @@
 // Якщо це так, то фільтруємо машини по бренду і повертаємо ті, що найбільш економні.
 
 function getMostFuelEfficientCars(cars, brand) {
-  // Ваш код
+  const allCarsAreEfficient = cars.every(car => car.fuelConsumption < 6);
+
+  if (allCarsAreEfficient) {
+    const filteredCars = cars.filter(car => car.brand === brand);
+    filteredCars.sort((a, b) => a.fuelConsumption - b.fuelConsumption);
+    
+    return filteredCars;
+  } else {
+    return []; 
+  }
 }
 
 // Приклад використання:
